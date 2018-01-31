04b1015e13bf725c1cd66999fdb87f40e536a3ab
David Corrado <davidcorrado@gmail.com>
2018-01-30 23:03:45 -0600

    initial project

---
+++ src/test/java/com/davidcorrado/kotlinrest/KotlinRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.davidcorrado.kotlinrest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KotlinRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
