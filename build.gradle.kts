30a07a47f46c6b1d7db51c1df73544f24f8ff055
davidcorrado <davidcorrado@gmail.com>
2021-09-23 19:28:26 -0500

    Project cleanups

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
 
     //Spring Boot
     implementation("org.springframework.boot:spring-boot-starter:${Versions.springBoot}")
-    implementation("org.springframework.boot:spring-boot-starter-web:${Versions.springBoot}")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:${Versions.springBoot}")
 
     //Kotlin
--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,6 +2,6 @@ object Versions {
     const val kotlin = "1.5.31"
     const val springBoot = "2.5.4"
     const val springDepManagement = "1.0.11.RELEASE"
-    const val postgres = "42.1.4"
-    const val jackson = "2.9.0"
+    const val postgres = "42.2.4"
+    const val jackson = "2.12.5"
 }
